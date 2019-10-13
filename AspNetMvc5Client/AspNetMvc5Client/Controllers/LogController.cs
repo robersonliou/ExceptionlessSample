2d31057ef5651d3b45d9b6b0430a3ed00ec20b7f
Roberson <wtx2043pp986@gmail.com>
2019-10-13 21:59:21 +0800

    create Log/Manually for log testing

---
+++ AspNetMvc5Client/AspNetMvc5Client/Controllers/LogController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Exceptionless;
+
+namespace AspNetMvc5Client.Controllers
+{
+    public class LogController : Controller
+    {
+        public ActionResult Manually()
+        {
+
+            ExceptionlessClient.Default.SubmitLog($"Loggin Now - {DateTime.Now}");
+            ExceptionlessClient.Default.SubmitLog("Log Source", $"Loggin Now - {DateTime.Now}", "Info");
+            ExceptionlessClient.Default.CreateLog("Log Source", $"Loggin Now - {DateTime.Now}", "Info")
+                .AddTags("Exceptionless").Submit();
+
+            return View();
+        }
+    }
+}
