84ea56dc293867780b8384dfd036aa332a2d3a26
Roberson <wtx2043pp986@gmail.com>
2019-10-13 21:51:51 +0800

    add Exception/Additional for addtional data sample

--- AspNetMvc5Client/AspNetMvc5Client/Controllers/ExceptionController.cs
+++ AspNetMvc5Client/AspNetMvc5Client/Controllers/ExceptionController.cs
@@ -26,5 +26,40 @@ namespace AspNetMvc5Client.Controllers
             }
             return View();
         }
+
+        public ActionResult Additional()
+        {
+            var order = GetFakeOrder();
+            try
+            {
+                throw new ApplicationException("Unable to create order from quote.");
+            }
+            catch (Exception ex)
+            {
+                ex.ToExceptionless()
+                    .SetReferenceId(Guid.NewGuid().ToString("N"))
+                    .AddObject(order, "Order", excludedPropertyNames: new[] {"CreditCardNumber"}, maxDepth: 1)
+                    .SetProperty("Quote", 122)
+                    .AddTags("Order")
+                    .MarkAsCritical()
+                    .SetGeo(42.595089, -88.444602)
+                    .Submit();
+            }
+
+            return View();
+        }
+
+        private object GetFakeOrder()
+        {
+            var order = new
+            {
+                orderId = 0,
+                orderItem = "Pencil",
+                orderName = "Roberson",
+                employeeName = "James",
+                CreditCardNumber = "1233-5678-8765-4321"
+            };
+            return order;
+        }
     }
 }
