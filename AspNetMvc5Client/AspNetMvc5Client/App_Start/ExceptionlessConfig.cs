d1690ebecbd58ee000e1070d526d76e045ae3eb6
Roberson <wtx2043pp986@gmail.com>
2019-10-13 21:14:51 +0800

    set your apikey and serverurl via code

--- AspNetMvc5Client/AspNetMvc5Client/App_Start/ExceptionlessConfig.cs
+++ AspNetMvc5Client/AspNetMvc5Client/App_Start/ExceptionlessConfig.cs
@@ -1,10 +1,16 @@
-﻿namespace AspNetMvc5Client
+﻿using Exceptionless;
+
+namespace AspNetMvc5Client
 {
     public class ExceptionlessConfig
     {
 
         public static void Register()
         {
+            var config = ExceptionlessClient.Default.Configuration;
+            //config.ApiKey = "your server api key ";
+            //config.ServerUrl = "http://localhost:5000";
+
 
         }
     }
