4dcff702fbbed14bd15375b01211bd897146f107
Roberson <wtx2043pp986@gmail.com>
2019-10-13 21:50:59 +0800

    add Exception/Manually for manually sending exception

--- AspNetMvc5Client/AspNetMvc5Client/Controllers/ExceptionController.cs
+++ AspNetMvc5Client/AspNetMvc5Client/Controllers/ExceptionController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Exceptionless;
 
 namespace AspNetMvc5Client.Controllers
 {
@@ -11,6 +12,19 @@ namespace AspNetMvc5Client.Controllers
         public ActionResult Null()
         {
             throw new NullReferenceException("send null exception to Exceptionless");
+        }
+
+        public ActionResult Manually()
+        {
+            try
+            {
+                new InvalidOperationException("manually throw exception");
+            }
+            catch (Exception ex)
+            {
+                ex.ToExceptionless().Submit();
+            }
+            return View();
         }
     }
 }
