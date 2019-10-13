edbb82ce7b9082d7d648dbbf8e67e24538d1b668
Roberson <wtx2043pp986@gmail.com>
2019-10-13 21:30:12 +0800

    create Exception/Null for testing exception auto report

---
+++ AspNetMvc5Client/AspNetMvc5Client/Controllers/ExceptionController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AspNetMvc5Client.Controllers
+{
+    public class ExceptionController : Controller
+    {
+        public ActionResult Null()
+        {
+            throw new NullReferenceException("send null exception to Exceptionless");
+        }
+    }
+}
