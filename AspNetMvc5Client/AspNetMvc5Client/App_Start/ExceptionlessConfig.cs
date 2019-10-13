05380050361fc7e3eaf870c8c65965bf8f01c927
Roberson <wtx2043pp986@gmail.com>
2019-10-13 22:56:55 +0800

    enable sending private info and test user sessions

--- AspNetMvc5Client/AspNetMvc5Client/App_Start/ExceptionlessConfig.cs
+++ AspNetMvc5Client/AspNetMvc5Client/App_Start/ExceptionlessConfig.cs
@@ -4,14 +4,13 @@ namespace AspNetMvc5Client
 {
     public class ExceptionlessConfig
     {
-
         public static void Register()
         {
             var config = ExceptionlessClient.Default.Configuration;
-            //config.ApiKey = "your server api key";
-            //config.ServerUrl = "http://localhost:5000";
-            config.IncludePrivateInformation = false;
-
+            config.ApiKey = "your server api key";
+            config.ServerUrl = "http://localhost:5000";
+//            config.IncludePrivateInformation = false;
+            config.UseSessions();
         }
     }
 }
