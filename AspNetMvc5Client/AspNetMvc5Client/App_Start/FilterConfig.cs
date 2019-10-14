316b6e5a1362905754f94f7e7e69db763e5c82ec
Roberson <wtx2043pp986@gmail.com>
2019-10-14 23:14:52 +0800

    create and register custom global filter for feature usage

--- AspNetMvc5Client/AspNetMvc5Client/App_Start/FilterConfig.cs
+++ AspNetMvc5Client/AspNetMvc5Client/App_Start/FilterConfig.cs
@@ -1,5 +1,6 @@
 ﻿using System.Web;
 using System.Web.Mvc;
+using AspNetMvc5Client.Filters;
 
 namespace AspNetMvc5Client
 {
@@ -8,6 +9,7 @@ namespace AspNetMvc5Client
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
+            filters.Add(new FeatureUsageAttribute());
         }
     }
 }
